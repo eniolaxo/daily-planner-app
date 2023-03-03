cba043663af9974351e3baa829c6414c9ddb6522
eniolaxo <117684350+eniolaxo@users.noreply.github.com>
2023-03-03 15:05:33 +0000

    fixed the css bootstrap styling of my planner by rearranging the size of columns, debugged the jquery

--- JS/script.js
+++ JS/script.js
@@ -1,39 +1,30 @@
-// Including this so that the document is loaded before executing any code.
-$(document).ready()
+$(document).ready(function(){
 
 // Display the current day at the top of the calender when a user opens the planner:
 // I selected the ID element and set its text to the current date formatted using the Moment.js library
 $("#currentDay").text(moment().format("MMM / D / YYYY"))
+
 // I attached a click event listener to all elements with the class 'saveBtn', so that when a user clicks 
 // the save button, the function reads and saves the value of the 'description' using hour as a key.
 $(".saveBtn").on("click", function(){
-var textArea = $(this).siblings(".description")
-var hours = $(this).siblings(".hour").text()
-localStorage.setItem (hours, textArea);
+var textArea = $(this).siblings(".description").val()
+var time = $(this).parent().attr("id")
+localStorage.setItem (time, textArea);
 })
 
 
 // Present timeblocks for standard business hours when the user scrolls down:
 // Defining variables for each of the time blocks in the HTML
-var nineAm = $("#9am");
-var tenAm = $("#10am");
-var elevenAm = $("#11am");
-var twelveAm = $("#12am");
-var onePm = $("#1pm");
-var twoPm = $("#2pm");
-var threePm = $("#3pm");
-var fourPm = $("#4pm");
-var fivePm = $("#5pm");
+function currentTime(){
+var currentHour = moment().hours();
+$(".time-block").each(function(){
+  var past = parseInt($(this).attr("id").split("-")[1])
+  // makea conditional (if statement) to check my time
+})
+}
 
-// Made a array variable of all the hours to make it more efficient and quicker to code through
-var hoursArray = [nineAm, tenAm, elevenAm, twelveAm, onePm, twoPm, threePm, fourPm, fivePm];
-
-// Looping through the hours array and get the value from local storage for each hour
-hoursArray.forEach(function(hour) {
-  var value = (hour);
-  // This targets the hour id
-  $("#" + hour).val(value);
-}); 
+currentTime()
+// create an applied timer and call out the function
 
 
 
@@ -41,10 +32,9 @@ hoursArray.forEach(function(hour) {
 // I need to define the current hour as of now:
 var timeNow = moment().hour();
 
+// Allow a user to enter an event when they click a timeblock
 
+// Save the event in local storage when the save button is clicked in that timeblock.
 
-// Allow a user to enter an event when they click a timeblock:
-
-// Save the event in local storage when the save button is clicked in that timeblock:
-
-// Persist events between refreshes of a page:
+// Persist events between refreshes of a page
+})
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # daily-planner-app
 Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+
+
+## Notes:
+I accidently saved and commited dozens of messages onto another repo thinking I was doing it to this repo, so I will list the messages of some of the commits I made onto here:
+
+-
+-
+-
+- debugged the code ...
+- added if statement to compare hours for past, present, future. then added css to change background colour
